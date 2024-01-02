1f77d10c2ca8ecc3c01a046fe013f7bfa262cee7
KozaPawel <p.koza99@o2.pl>
2024-01-02 20:53:04 +0100

    #2 - Add auth screens

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import { Colors } from "./constants/colors";
+
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.background },
+        headerTintColor: Colors.text,
+        contentStyle: { backgroundColor: Colors.background },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="dark" />
+
+      <Navigation />
+    </>
+  );
+}
---
+++ components/auth/AuthContent.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { Alert, StyleSheet, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import FlatButton from "../ui/FlatButton";
+import AuthForm from "./AuthForm";
+
+function AuthContent({ isLogin, onAuthenticate }) {
+  const navigation = useNavigation();
+
+  const [credentialsInvalid, setCredentialsInvalid] = useState({
+    email: false,
+    password: false,
+    confirmPassword: false,
+  });
+
+  function switchAuthModeHandler() {
+    if (isLogin) {
+      navigation.replace("Signup");
+    } else {
+      navigation.replace("Login");
+    }
+  }
+
+  function submitHandler(credentials) {
+    let { email, password, confirmPassword } = credentials;
+
+    email = email.trim();
+    password = password.trim();
+
+    const emailIsValid = email.includes("@");
+    const passwordIsValid = password.length > 6;
+    const passwordsAreEqual = password === confirmPassword;
+
+    if (!emailIsValid || !passwordIsValid || (!isLogin && !passwordsAreEqual)) {
+      Alert.alert("Invalid input", "Please check your entered credentials.");
+      setCredentialsInvalid({
+        email: !emailIsValid,
+        password: !passwordIsValid,
+        confirmPassword: !passwordIsValid || !passwordsAreEqual,
+      });
+      return;
+    }
+    onAuthenticate({ email, password });
+  }
+
+  return (
+    <View style={styles.authContent}>
+      <AuthForm
+        isLogin={isLogin}
+        onSubmit={submitHandler}
+        credentialsInvalid={credentialsInvalid}
+      />
+      <View style={styles.buttons}>
+        <FlatButton onPress={switchAuthModeHandler}>
+          {isLogin
+            ? "Don't have an account yet? Sign up"
+            : "Have an account already? Log in"}
+        </FlatButton>
+      </View>
+    </View>
+  );
+}
+
+export default AuthContent;
+
+const styles = StyleSheet.create({
+  authContent: {
+    marginTop: 64,
+    marginHorizontal: 32,
+    padding: 16,
+  },
+  buttons: {
+    marginTop: 8,
+  },
+});
